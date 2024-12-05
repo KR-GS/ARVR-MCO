8d1cfddb2db74616db0688cd70196834f38e58d8
Kaci Reenea Santos <107488670+KR-GS@users.noreply.github.com>
2024-12-05 20:07:58 +0800

    Working Gestures, elevator buttons, and scene changes pt 16

---
+++ ARVR MCO/Library/Bee/artifacts/Android/il2cppOutput/cpp/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+IL2CPP_EXTERN_C_CONST int32_t* g_FieldOffsetTable[];
+IL2CPP_EXTERN_C_CONST Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	8886,
+	g_Il2CppGenericTypes,
+	5484,
+	g_Il2CppGenericInstTable,
+	68171,
+	g_Il2CppGenericMethodFunctions,
+	25550,
+	g_Il2CppTypeTable,
+	81402,
+	g_Il2CppMethodSpecTable,
+	7564,
+	g_FieldOffsetTable,
+	7564,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
